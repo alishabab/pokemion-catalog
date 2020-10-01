d4101ad714683374615ffe490e7e375927550991
Shabab <shababsaifi@gmail.com>
2020-10-01 20:45:25 +0530

    Added Pokemmon and PokemonList components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import PokemonList from '../PokemonList/PokemonList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <PokemonList />
     </div>
   );
 }
---
+++ src/components/PokemonList/PokemonList.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import classes from './PokemonList.module.css';
+
+import Pokemon from '../Pokemon/Pokemon';
+
+const pokemons = [
+  {
+    id: 1,
+    src: 'https://i.pinimg.com/originals/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.png',
+    name: 'picachu',
+  },
+  {
+    id: 2,
+    src: 'https://i.pinimg.com/originals/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.png',
+    name: 'picachu',
+  },
+  {
+    id: 3,
+    src: 'https://i.pinimg.com/originals/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.png',
+    name: 'picachu',
+  },
+  {
+    id: 4,
+    src: 'https://i.pinimg.com/originals/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.png',
+    name: 'picachu',
+  },
+  {
+    id: 5,
+    src: 'https://i.pinimg.com/originals/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.png',
+    name: 'picachu',
+  },
+  {
+    id: 6,
+    src: 'https://i.pinimg.com/originals/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.png',
+    name: 'picachu',
+  },
+];
+
+const PokemonList = () => (
+  <div className={classes.PokemonList}>
+    { pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+  </div>
+);
+
+export default PokemonList;
