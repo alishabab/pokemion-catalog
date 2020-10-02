f185850140bf21c6a2be93aa468fb292fe5a341b
Shabab <shababsaifi@gmail.com>
2020-10-02 17:21:59 +0530

    added methods to get pokemons data from pokeApi

---
+++ src/api/index.js
@@ -0,0 +1,66 @@
+import {
+  getPokemonsPending,
+  getPokemonsSuccess,
+  getPokemonsError,
+  getSinglePokemonError,
+  getSinglePokemonPending,
+  getSinglePokemonSuccess,
+  changeType,
+} from '../actions/index';
+
+const baseUrl = 'https://pokeapi.co/api/v2';
+
+const getData = url => {
+  fetch(url).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    return res.status;
+  })
+    .catch(err => console.log(err));
+};
+
+const getPokemon = name => async dispatch => {
+  dispatch(getSinglePokemonPending());
+  try {
+    const response = await getData(`${baseUrl}/pokemon/${name}`);
+    const pokemon = {
+      name: response.name,
+      abilities: response.abilities,
+      sprites: response.sprites,
+      stats: response.stats,
+    };
+    dispatch(getSinglePokemonSuccess(pokemon));
+    return pokemon;
+  } catch (e) {
+    dispatch(getSinglePokemonError(e));
+    return e;
+  }
+};
+
+const getPokemons = type => async dispatch => {
+  dispatch(getPokemonsPending());
+  try {
+    const response = await getData(`${baseUrl}/type/${type}`);
+    const pokemons = response.pokemon.map(async pok => {
+      const res = await fetch(pok.pokemon.url);
+      return res.json();
+    });
+    const pokemonsData = await Promise.all(pokemons);
+    const payload = pokemonsData.map(data => ({
+      name: data.name,
+      image: data.sprites.front_default,
+    }));
+    dispatch(getPokemonsSuccess(payload));
+    dispatch(changeType(type));
+    return response;
+  } catch (e) {
+    dispatch(getPokemonsError(e));
+    return e;
+  }
+};
+
+export default {
+  getPokemon,
+  getPokemons,
+};
